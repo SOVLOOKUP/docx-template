48a3e336c91e558aac2af79f4a92e6a681608a17
fan xia <gonorth@qq.com>
2024-04-02 00:02:55 +0800

    ok

--- src/main/java/com/sovlookup/docx/template/DocxTemplate.java
+++ src/main/java/com/sovlookup/docx/template/DocxTemplate.java
@@ -1,14 +1,10 @@
-package com.deepoove.poi.cli;
+package com.sovlookup.docx.template;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
 
-import com.beust.jcommander.JCommander;
-import com.beust.jcommander.Parameter;
 import com.deepoove.poi.XWPFTemplate;
 import com.deepoove.poi.config.Configure;
 import com.deepoove.poi.config.ConfigureBuilder;
@@ -19,27 +15,25 @@ import com.deepoove.poi.jsonmodel.support.GsonPreRenderDataCastor;
 import com.deepoove.poi.plugin.comment.CommentRenderPolicy;
 import com.deepoove.poi.plugin.highlight.HighlightRenderData;
 import com.deepoove.poi.plugin.highlight.HighlightRenderPolicy;
-import com.deepoove.poi.plugin.markdown.FileMarkdownRenderData;
 import com.deepoove.poi.plugin.markdown.MarkdownRenderData;
 import com.deepoove.poi.plugin.markdown.MarkdownRenderPolicy;
 import com.deepoove.poi.plugin.toc.TOCRenderPolicy;
 import com.deepoove.poi.policy.AttachmentRenderPolicy;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.typeadapters.RuntimeTypeAdapterFactory;
 
 public class DocxTemplate {
     private GsonHandler gsonHandler;
     private Configure configure;
+    private static final Type TYPE = new TypeToken<Map<String, Object>>() {
+    }.getType();
     {
         ConfigureBuilder builder = Configure.builder();
         this.gsonHandler = new DefaultGsonHandler() {
             @Override
             protected RuntimeTypeAdapterFactory<RenderData> createRenderTypeAdapter(boolean readable) {
                 return super.createRenderTypeAdapter(readable).registerSubtype(MarkdownRenderData.class, "markdown")
-                        .registerSubtype(HighlightRenderData.class, "code")
-                        .registerSubtype(FileMarkdownRenderData.class, "markdown-file");
+                        .registerSubtype(HighlightRenderData.class, "code");
             }
 
             @Override
@@ -50,8 +44,7 @@ public class DocxTemplate {
                 typeAdapter.add(RuntimeTypeAdapterFactory.of(HighlightRenderData.class, "type", readable)
                         .registerSubtype(HighlightRenderData.class, "code"));
                 typeAdapter.add(RuntimeTypeAdapterFactory.of(MarkdownRenderData.class, "type", readable)
-                        .registerSubtype(MarkdownRenderData.class, "markdown")
-                        .registerSubtype(FileMarkdownRenderData.class, "markdown-file"));
+                        .registerSubtype(MarkdownRenderData.class, "markdown"));
                 return typeAdapter;
             }
         };
@@ -67,7 +60,7 @@ public class DocxTemplate {
         this.configure = builder.build();
     }
 
-    public void run(String template, String output, String jsonStr) {
+    public void run(String template, String output, String jsonStr) throws IOException {
         XWPFTemplate.compile(template,
                 this.configure)
                 .render(this.gsonHandler.castJsonToType(jsonStr, TYPE))
